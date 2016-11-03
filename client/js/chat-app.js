e4c20027b3580c73694813624642af841171217f
Navi Kaur <nkaur901@gmail.com>
2016-11-02 23:25:35 -0400

    created a stylesheet to style the chat room

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -3,62 +3,61 @@
 
   window.chat = window.chat || {};
 
-window.chat.listenForMessages(function messageHandler(data) {
-  $('.messages')
-    .append('<p>' + data.message + '</p>')
+  window.chat.listenForMessages(function messageHandler(data) {
+    $('.messages')
+    .append('<p>' + data.username + ': ' + data.message + '</p>')
+  });
 
-console.log(data);
+  var token;
+  $('.login')
+    .on('submit', function submitLogin(event) {
+      event.preventDefault();
 
+      $.ajax ({
+        url: '/login',
+        method: 'POST',
+        data: JSON.stringify({username: $('.username').val() }),
+        // dataType: 'text',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
 
-});
+      .done(function successHandler (data) {
+        token = data.token;
+        $('.login').trigger('reset');
+      })
 
-var token;
-$('.login')
-  .on('submit', function submitLogin(event) {
-    event.preventDefault();
-    $.ajax ({
-      url: '/login',
-      method: 'POST',
-      data: JSON.stringify({username: $('.username').val() }),
-      // dataType: 'text',
-      headers: {
-        'Content-Type': 'application/json'
-      }
+      .fail(function errorHandler(xhr, errorType) {
+        console.log(xhr, errorType);
+      })
+    });
+
+  $('.send-message')
+    .on('submit', function submitMessage(event) {
+      event.preventDefault();
+      $.ajax ({
+        url: '/chat',
+        method: 'POST',
+        data: JSON.stringify({message: $('.message').val() }),
+        // dataType: 'text',
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json'
+        }
+      })
+    .done(function successHandler (data) {
+      $('.send-message').trigger('reset');
+      // token = data.myToken;
     })
-  .done(function successHandler (data) {
-    console.log('it worked', data);
-    token = data.token;
-  })
-  .fail(function errorHandler(xhr) {
-    console.log(xhr, 'you got an error');
-  })
-});
-
-$('.send-message')
-  .on('submit', function submitMessage(event) {
-    event.preventDefault();
-    $.ajax ({
-      url: '/chat',
-      method: 'POST',
-      data: JSON.stringify({message: $('.message').val() }),
-      // dataType: 'text',
-      headers: {
-        Authorization: token,
-        'Content-Type': 'application/json'
-
-      }
+    .fail(function errorHandler(xhr, errorType) {
+      // if (xhr.status === 404) {
+      //     $('body').append('<p>Sorry, cannot find that file!</p>');
+      // } else (xhr.status === 500) {
+      //     $('body').append('<p>Sorry, the server is down!</p>');
+      // }
+        console.log(xhr, errorType);
     })
-  .done(function successHandler (data) {
-    console.log('submit was successful', data);
-    // token = data.myToken;
-  })
-  .fail(function errorHandler(xhr) {
-    console.log(xhr, 'you got an error when you submitted');
-  })
-});
-
-
-
-
+   });
 
 }());
